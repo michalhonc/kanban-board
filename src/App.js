777604b71ea75aa0af2593416472eece4ae8742a
michalhonc <honc355@gmail.com>
2018-06-05 11:42:32 +0200

    App change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Board from '../containers/board';
-import AddBoard from '../containers/add_board';
+import Board from './containers/board';
+import AddBoard from './containers/add_board';
 export default () => {
    return(
       <div className="app">
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import './App.css';
-import App from './components/app';
+import App from './App';
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 import throttle from 'lodash/throttle';
